aa674b21e45fa517901cc4b9a0338767345e426f
nicholaslee97 <nicholas.cy.lee@gmail.com>
2022-01-25 17:45:21 +0800

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,19 +1,19 @@
 module.exports = {
 	name: 'interactionCreate',
-	execute(interaction, client) {
+	async execute(interaction, client) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 		if (!interaction.isCommand()) return;
 		try {
-            console.log(client.commands);
+			console.log(client.commands);
 			const command = client.commands.get(interaction.commandName);
 			if (!command) return;
 
 
-			command.execute(interaction);
+			await command.execute(interaction);
 		}
 		catch (error) {
 			console.error(error);
-			interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	},
 };
