3ca2468f24c5b297fb3379f2c5ef9122fc2f7c96
nicholaslee97 <nicholas.cy.lee@gmail.com>
2022-01-26 20:30:13 +0800

    Update random_number.js

--- commands/random_number.js
+++ commands/random_number.js
@@ -7,13 +7,24 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('god_number')
-				.setDescription('random a god number')),
+				.setDescription('random a god number'))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('num')
+				.setDescription('random between two numbers')
+				.addIntegerOption(option => option.setName('min').setDescription('Min number'))
+				.addIntegerOption(option => option.setName('max').setDescription('Max number'))),
 
 	async execute(interaction) {
 		if (interaction.options.getSubcommand() === 'god_number') {
 			const num = Math.round(Math.random() * 899999 + 100000);
 			console.log(num);
 			await interaction.reply('123' + num);
+		}
+		else if (interaction.options.getSubcommand() === 'num') {
+			const min = interaction.options.getInteger('min');
+			const max = interaction.options.getInteger('max');
+			await interaction.reply('The number is ' + Math.round(Math.random() * (max - min) + min));
 		}
 	},
 };
